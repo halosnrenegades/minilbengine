003fb914cb85a0005d6345f2147f333166de40dc
halosnrenegades <halosnrenegades@gmail.com>
2026-04-28 12:50:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,5 @@
-const express = require("express");
-const mongoose = require("mongoose");
-
-const app = express();
-
-// connect to MongoDB
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("Mongo error:", err));
-
-// basic route
-app.get("/", (req, res) => {
-  res.json({ status: "ok", message: "engine alive" });
-});
-
 const PORT = process.env.PORT || 10000;
+
 app.listen(PORT, () => {
   console.log(`Looney Bin Engine running on port ${PORT}`);
 });
