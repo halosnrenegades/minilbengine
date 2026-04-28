91ff15ee860047fd758ba0c6c6637c4d6df699df
halosnrenegades <halosnrenegades@gmail.com>
2026-04-28 04:56:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Mini LB Engine is running");
+});
+
+app.get("/api/jobs", (req, res) => {
+  res.json({ status: "ok", message: "engine alive" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
